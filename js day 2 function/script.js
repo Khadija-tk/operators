9a43694fedad1af5ce8c2d412906c9c388f9fd25
Khadija <1011hadija@gmail.com>
2024-12-07 06:13:09 -0800

    Add files via upload

---
+++ js day 2 function/script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById('ok').style.background ='palevioletred'
+    document.getElementById('heading').style.textAlign = 'center'
+    document.getElementById('heading').style.fontFamily = 'cursive'
+    document.getElementById('hii').style.fontFamily = 'cursive'
+}
+window.alert('Please go back, you are banned on this site').style.background = 'pink'
